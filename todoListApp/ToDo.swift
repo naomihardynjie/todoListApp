e8dce6100e8bbb12afe128696a8b4f6c378c602e
Naomi <naominjie12@gmail.com>
2021-06-14 18:09:01 -0400

    Iteration 0

---
+++ todoListApp/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  todoListApp
+//
+//  Created by Naomi Hardy-Njie on 6/14/21.
+//
+
+import UIKit
+
+class ToDo {
+var name = ""
+var important = false
+}
---
+++ todoListApp/ToDoTableViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ToDoTableViewController.swift
+//  todoListApp
+//
+//  Created by Naomi Hardy-Njie on 6/14/21.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo] {
+      
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        
+        return [swift, dog]
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+//        return 0
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let toDo = toDos[indexPath.row]
+
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        
+        
+        return cell
+
+    }
+
+
+    
+   
+
+    
+
+   
+
+}
